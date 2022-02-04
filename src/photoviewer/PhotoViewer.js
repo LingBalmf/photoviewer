12f4bb070ce362deeb77a5b5916bb36a0506f593
Ling Balmforth <Ling.Balmforth@softwire.com>
2022-02-04 13:04:31 +0000

    Extended code to handle 50 images

--- src/photoviewer/PhotoViewer.js
+++ src/photoviewer/PhotoViewer.js
@@ -4,17 +4,22 @@ import './PhotoViewer.css';
 
 
 export function PhotoViewer(props) {
-    const url = props.urls[0];
+    const urls = props.urls;
     const [selectedUrl, setSelectedUrl] = useState();
+    const components = [];
+
+    for (let i = 0; i < 50; i++) {
+        const url = urls[i];
+        components.push(
+            <img className="photoviewer-image" key={url} src={url} alt="LovelyImage" onClick={function () { setSelectedUrl(url); }} />
+        );
+    }
 
     return (
         <div>
             <h2>Selected: {selectedUrl}</h2>
-            <div>
-            <img className="photoviewer-image" key={url} src={url} alt="Lovely" onClick={function () {setSelectedUrl(url); }} />
-            </div>
+            {components}
         </div>
-    )
+    );
 }
-
 
